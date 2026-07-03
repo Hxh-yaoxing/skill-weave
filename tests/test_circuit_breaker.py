129346557fa98740f7782140d93b28c751006715
曜行 <2365360230@qq.com>
2026-07-03 21:39:23 +0800

    Fix test_circuit_breaker: absolute import

--- tests/test_circuit_breaker.py
+++ tests/test_circuit_breaker.py
@@ -8,7 +8,7 @@ import sys, os
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 
-from circuit_breaker import CircuitBreaker, CircuitState
+from skill_weave.circuit_breaker import CircuitBreaker, CircuitState
 
 
 class TestCircuitBreaker:
